d7bfdef354d5c6642adaf39ce553d924c202f417
linqining <linqining@outlook.com>
2026-02-08 13:24:28 +0800

    feat: move code

--- src/barnett_smart_protocol/mod.rs
+++ src/barnett_smart_protocol/mod.rs
@@ -1,11 +1,12 @@
-use super::{to_bytes, BarnettSmartProtocol};
+use std::hash::Hash;
+use super::{to_bytes};
 use super::{Mask, Remask, Reveal};
 
 use crate::error::CardProtocolError;
 
 use anyhow::Result;
 use ark_ec::{AffineRepr, CurveGroup};
-use ark_ff::{ One};
+use ark_ff::{Field, One};
 use ark_marlin::rng::FiatShamirRng;
 use ark_std::rand::Rng;
 use ark_ff::Zero;
@@ -23,8 +24,8 @@ use proof_base::zkp::{
     ArgumentOfKnowledge,
 };
 use std::marker::PhantomData;
-use ark_serialize::{ CanonicalSerialize};
-use ark_std::ops::Mul;
+use ark_serialize::{CanonicalDeserialize, CanonicalSerialize};
+use ark_std::ops::{Add,Mul};
 
 // mod key_ownership;
 mod masking;
@@ -85,6 +86,169 @@ const MASKING_RNG_SEED: &'static [u8] = b"Masking Proof";
 const REMASKING_RNG_SEED: &'static [u8] = b"Remasking Proof";
 const REVEAL_RNG_SEED: &'static [u8] = b"Reveal Proof";
 const SHUFFLE_RNG_SEED: &'static [u8] = b"Shuffle Proof";
+
+/// Mental Poker protocol based on the one described by Barnett and Smart (2003).
+/// The protocol has been modified to make use of the argument of a correct shuffle presented
+/// by Bayer and Groth (2014).
+pub trait BarnettSmartProtocol {
+    // Cryptography
+    type Scalar: Field;
+    type Parameters;
+    type PlayerPublicKey: CanonicalDeserialize + CanonicalSerialize;
+    type PlayerSecretKey;
+    type AggregatePublicKey: CanonicalDeserialize + CanonicalSerialize;
+    type Enc: HomomorphicEncryptionScheme<Self::Scalar>;
+    type Comm: HomomorphicCommitmentScheme<Self::Scalar>;
+
+    // Cards
+    type Card: Copy
+    + Clone
+    + Mask<Self::Scalar, Self::Enc>
+    + CanonicalDeserialize
+    + CanonicalSerialize
+    + Hash
+    + Eq;
+    type MaskedCard: Remask<Self::Scalar, Self::Enc> + CanonicalDeserialize + CanonicalSerialize;
+    type RevealToken: Add
+    + Reveal<Self::Scalar, Self::Enc>
+    + Mul<Self::Scalar, Output = Self::RevealToken>
+    + CanonicalDeserialize
+    + CanonicalSerialize;
+
+    // Proofs
+    type ZKProofKeyOwnership: CanonicalDeserialize + CanonicalSerialize;
+    type ZKProofMasking: CanonicalDeserialize + CanonicalSerialize;
+    type ZKProofRemasking: CanonicalDeserialize + CanonicalSerialize;
+    type ZKProofReveal: CanonicalDeserialize + CanonicalSerialize;
+    type ZKProofShuffle: CanonicalDeserialize + CanonicalSerialize;
+
+    /// Randomly produce the scheme parameters
+    fn setup<R: Rng>(
+        rng: &mut R,
+        m: usize,
+        n: usize,
+    ) -> std::result::Result<Self::Parameters, CardProtocolError>;
+
+    /// Generate keys for a player.
+    fn player_keygen<R: Rng>(
+        rng: &mut R,
+        pp: &Self::Parameters,
+    ) -> std::result::Result<(Self::PlayerPublicKey, Self::PlayerSecretKey), CardProtocolError>;
+
+    /// Prove in zero knowledge that the owner of a public key `pk` knows the corresponding secret key `sk`
+    fn prove_key_ownership<B: CanonicalSerialize, R: Rng>(
+        rng: &mut R,
+        pp: &Self::Parameters,
+        pk: &Self::PlayerPublicKey,
+        sk: &Self::PlayerSecretKey,
+        player_public_info: &B,
+    ) -> std::result::Result<Self::ZKProofKeyOwnership, CryptoError>;
+
+    /// Verify a proof od key ownership
+    fn verify_key_ownership<B: CanonicalSerialize>(
+        pp: &Self::Parameters,
+        pk: &Self::PlayerPublicKey,
+        player_public_info: &B,
+        proof: &Self::ZKProofKeyOwnership,
+    ) -> std::result::Result<(), CryptoError>;
+
+    /// Use all the public keys and zk-proofs to compute a verified aggregate public key
+    fn compute_aggregate_key<B: CanonicalSerialize>(
+        pp: &Self::Parameters,
+        player_keys_proof_info: &Vec<(Self::PlayerPublicKey, Self::ZKProofKeyOwnership, B)>,
+    ) -> std::result::Result<Self::AggregatePublicKey, CardProtocolError>;
+
+    /// Use the shared public key and a (private) random scalar `alpha` to mask a card.
+    /// Returns a masked card and a zk-proof that the masking operation was applied correctly.
+    fn mask<R: Rng>(
+        rng: &mut R,
+        pp: &Self::Parameters,
+        shared_key: &Self::AggregatePublicKey,
+        original_card: &Self::Card,
+        alpha: &Self::Scalar,
+    ) -> std::result::Result<(Self::MaskedCard, Self::ZKProofMasking), CardProtocolError>;
+
+    /// Verify a proof of masking
+    fn verify_mask(
+        pp: &Self::Parameters,
+        shared_key: &Self::AggregatePublicKey,
+        card: &Self::Card,
+        masked_card: &Self::MaskedCard,
+        proof: &Self::ZKProofMasking,
+    ) -> std::result::Result<(), CryptoError>;
+
+    /// Use the shared public key and a (private) random scalar `alpha` to remask a masked card.
+    /// Returns a masked card and a zk-proof that the remasking operation was applied correctly.
+    fn remask<R: Rng>(
+        rng: &mut R,
+        pp: &Self::Parameters,
+        shared_key: &Self::AggregatePublicKey,
+        original_masked: &Self::MaskedCard,
+        alpha: &Self::Scalar,
+    ) -> std::result::Result<(Self::MaskedCard, Self::ZKProofRemasking), CardProtocolError>;
+
+    /// Verify a proof of remasking
+    fn verify_remask(
+        pp: &Self::Parameters,
+        shared_key: &Self::AggregatePublicKey,
+        original_masked: &Self::MaskedCard,
+        remasked: &Self::MaskedCard,
+        proof: &Self::ZKProofRemasking,
+    ) -> std::result::Result<(), CryptoError>;
+
+    /// Players can use this function to compute their reveal token for a given masked card.
+    /// The token is accompanied by a proof that it is a valid reveal for the specified card issued
+    /// by the player who ran the computation.
+    fn compute_reveal_token<R: Rng>(
+        rng: &mut R,
+        pp: &Self::Parameters,
+        sk: &Self::PlayerSecretKey,
+        pk: &Self::PlayerPublicKey,
+        masked_card: &Self::MaskedCard,
+    ) -> std::result::Result<(Self::RevealToken, Self::ZKProofReveal), CardProtocolError>;
+
+    /// Verify a proof of correctly computed reveal token
+    fn verify_reveal(
+        pp: &Self::Parameters,
+        pk: &Self::PlayerPublicKey,
+        reveal_token: &Self::RevealToken,
+        masked_card: &Self::MaskedCard,
+        proof: &Self::ZKProofReveal,
+    ) -> std::result::Result<(), CryptoError>;
+
+    /// After collecting all the necessary reveal tokens and proofs that these are correctly issued,
+    /// players can unmask a masked card to recover the underlying card.
+    fn unmask(
+        pp: &Self::Parameters,
+        decryption_key: &Vec<(
+            Self::RevealToken,
+            Self::ZKProofReveal,
+            Self::PlayerPublicKey,
+        )>,
+        masked_card: &Self::MaskedCard,
+    ) -> std::result::Result<Self::Card, CardProtocolError>;
+
+    /// Shuffle and remask a deck of masked cards using a player-chosen permutation and vector of
+    /// masking factors.
+    fn shuffle_and_remask<R: Rng>(
+        rng: &mut R,
+        pp: &Self::Parameters,
+        shared_key: &Self::AggregatePublicKey,
+        deck: &Vec<Self::MaskedCard>,
+        masking_factors: &Vec<Self::Scalar>,
+        permutation: &Permutation,
+    ) -> std::result::Result<(Vec<Self::MaskedCard>, Self::ZKProofShuffle), CardProtocolError>;
+
+    /// Verify a proof of correct shuffle
+    fn verify_shuffle(
+        pp: &Self::Parameters,
+        shared_key: &Self::AggregatePublicKey,
+        original_deck: &Vec<Self::MaskedCard>,
+        shuffled_deck: &Vec<Self::MaskedCard>,
+        proof: &Self::ZKProofShuffle,
+    ) -> std::result::Result<(), CryptoError>;
+}
+
 
 impl<C: CurveGroup> BarnettSmartProtocol for DLCards<C> {
     type Scalar = C::ScalarField;
